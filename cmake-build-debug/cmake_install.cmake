5385c45b1b62cc10847ffaa5c29cbf4890ae6c87
Yurii <yuriyperederiyyy@gmail.com>
2024-05-10 01:43:15 +0300

    Commit

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -47,6 +47,11 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("D:/Work/Coding/PROJECTS/DesignPatterns/cmake-build-debug/Singleton/cmake_install.cmake")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Work/Coding/PROJECTS/DesignPatterns/cmake-build-debug/Command/cmake_install.cmake")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
