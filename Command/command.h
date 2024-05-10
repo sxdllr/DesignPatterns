84f49e1e8dd1fa978465a4e028c0db72c3c422c2
Yurii <yuriyperederiyyy@gmail.com>
2024-05-11 00:35:38 +0300

    Command

--- Command/command.h
+++ Command/command.h
@@ -1,7 +1,12 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
+#include <random>
+#include <cmath>
 #include <utility>
+
+float Rnd();
 
 struct Character
 {
@@ -33,10 +38,29 @@ class Move: public Command
 public:
     void execute(Character& character) override
     {
-        std::cout << character.name << " moved\n";
+        float rnd_x = Rnd();
+        float rnd_y = Rnd();
+        character.Move(rnd_x, rnd_y);
+        _x = rnd_x;
+        _y = rnd_y;
+        std::cout << character.name << " moved to ("
+            << _x << ";" << _y << ") position\n";
     }
     void undo(Character& character) override
     {
-
+        character.Move(-_x,-_y);
+        std::cout << character.name << " undo to ("
+                  << -_x << ";" << -_y << ") position\n";
     }
+private:
+    float _x;
+    float _y;
 };
+
+float Rnd() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<float> distribution(1.f, 10.f);
+    float rnd = std::ceil(distribution(gen) * 10) / 10;;
+    return rnd;
+}
--- Command/main.cpp
+++ Command/main.cpp
@@ -1,12 +1,28 @@
 #include "command.h"
+#include <memory>
 
 //way to take objects && model them as commands that execute actions
 
 int main()
 {
     Character ch("Yurii");
-    Move w;
-    w.execute(ch);
+    std::vector<std::unique_ptr<Command>> command_queue;
+
+    command_queue.push_back(std::make_unique<Move>());
+    command_queue.push_back(std::make_unique<Move>());
+    command_queue.push_back(std::make_unique<Move>());
+    command_queue.push_back(std::make_unique<Move>());
+
+    for(auto& it : command_queue)
+    {
+        it->execute(ch);
+    }
+
+    std::cout << "\n";
+
+    for(auto it = command_queue.rbegin(); it != command_queue.rend(); ++it) {
+        (*it)->undo(ch);
+    }
 
     return EXIT_SUCCESS;
 }
